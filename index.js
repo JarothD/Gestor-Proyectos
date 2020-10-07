e036970e72b08185adb750240657cf7e0f4d7ed9
JarothD <jarothd@gmail.com>
2020-10-07 17:26:05 -0500

    ajustes para deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json ({ extended: true }));
 
 
 // Puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.port || 5000;
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 // Arrancar la app
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`el servidor esta funcionando en el puerto ${port}`)
 }); 
