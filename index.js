c57720e1cbdede11e8ed617f26178adc815df9a9
JarothD <jarothd@gmail.com>
2020-10-12 13:51:54 -0500

    cambio a package.json 3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json ({ extended: true }));
 
 
 // Puerto de la app
-const PORT = process.env.PORT;// || 5000;
+const port = process.env.PORT;// || 5000;
 //app.listen(process.env.PORT || 5000);
 
 // Importar Rutas
@@ -26,5 +26,5 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 // Arrancar la app
 app.listen(PORT, () => {
-    console.log(`el servidor esta funcionando en el puerto ${PORT}`, this.address().port, app.settings.env)
+    console.log(`el servidor esta funcionando en el puerto ${PORT}`)
 }); 
